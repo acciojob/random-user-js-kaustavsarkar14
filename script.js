1f5213e76c933095d9224ff28c4b768257628f3a
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-10-25 11:20:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+async function getData(){
+    const response = await fetch('https://randomuser.me/api/')
+    const result = await response.json()
+    const userData = result.results[0]
+    console.log(userData)
+    photo.src = userData.picture.large
+    info.innerText = ''
+    age.addEventListener('click', e=>{
+        info.innerText = userData.dob.age
+    })
+    email.addEventListener('click', e=>{
+        info.innerText = userData.email
+    })
+    phone.addEventListener('click', e=>{
+        info.innerText = userData.cell
+    })
+}
+
+getData()
+
